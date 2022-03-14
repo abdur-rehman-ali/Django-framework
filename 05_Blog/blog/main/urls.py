620803d642bd58d2f66c675f9f7758e79411ba9f
abdur-rehman-ali <alijoyia911@gmail.com>
2022-03-14 06:22:54 +0500

    createView

--- 05_Blog/blog/main/urls.py
+++ 05_Blog/blog/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index.as_view(),name='index'),
+    path('article/create',views.postCreate.as_view(),name='postCreate'),
     path('article/detail/<int:pk>',views.postDetail.as_view(),name='postDetail'),
 ]
--- 05_Blog/blog/main/views.py
+++ 05_Blog/blog/main/views.py
@@ -1,7 +1,9 @@
+from dataclasses import fields
 from statistics import mode
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 # Create your views here.
@@ -13,4 +15,9 @@ class index(ListView):
 class postDetail(DetailView):
     model = Post
     template_name = 'updatePost.html'
-    
+
+class postCreate(CreateView):
+    model = Post
+    template_name = 'createPost.html'
+    fields = ['title','description','author']
+    success_url = reverse_lazy('index')
