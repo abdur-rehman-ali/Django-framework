a262430a7214f8db595ca6a3becd39909ff139b6
abdur-rehman-ali <alijoyia911@gmail.com>
2022-03-14 06:06:59 +0500

    Detail view

--- 05_Blog/blog/main/urls.py
+++ 05_Blog/blog/main/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index.as_view(),name='index'),
+    path('article/detail/<int:pk>',views.postDetail.as_view(),name='postDetail'),
 ]
--- 05_Blog/blog/main/views.py
+++ 05_Blog/blog/main/views.py
@@ -1,6 +1,7 @@
 from statistics import mode
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Post
 
 # Create your views here.
@@ -8,3 +9,8 @@ class index(ListView):
     model = Post
     template_name = 'index.html'
     ordering = ['-id']
+
+class postDetail(DetailView):
+    model = Post
+    template_name = 'updatePost.html'
+    
