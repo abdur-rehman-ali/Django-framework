993e5243e2510afbdf97d9da61838f1a60070d6b
abdur-rehman-ali <alijoyia911@gmail.com>
2022-03-14 09:37:15 +0500

    post form updated

---
+++ 05_Blog/blog/main/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Post
+
+class postForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ("title","description","author")
+        widgets ={
+            'title': forms.TextInput(attrs={
+                'class':'form-control',
+            }),
+            'description': forms.Textarea(attrs={
+                'class':'form-control',
+            }),
+            'author': forms.Select(attrs={
+                'class':'form-control',
+            }),
+        }
--- 05_Blog/blog/main/views.py
+++ 05_Blog/blog/main/views.py
@@ -5,6 +5,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from .models import Post
+from .forms import postForm
 
 # Create your views here.
 class index(ListView):
@@ -18,6 +19,6 @@ class postDetail(DetailView):
 
 class postCreate(CreateView):
     model = Post
+    form_class = postForm
     template_name = 'createPost.html'
-    fields = ['title','description','author']
     success_url = reverse_lazy('index')
